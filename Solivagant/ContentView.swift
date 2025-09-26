9b36c79b021b2a4f19c32cdb1983fa0b19bec631
Andrew Straight <devArtsw0rd7@protonmail.com>
2025-09-25 19:46:15 -0500

    Initial Commit

---
+++ Solivagant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Solivagant
+//
+//  Created by artsw0rd on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Solivagant/SolivagantApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolivagantApp.swift
+//  Solivagant
+//
+//  Created by artsw0rd on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SolivagantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
